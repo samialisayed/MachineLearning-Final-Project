7035fa28843a30d21c9042e75af1f4892260bf27
Tova Schwartz <tovagschwartz@gmail.com>
2020-04-27 12:08:02 -0400

    added function to plot hist of variables

--- project.py
+++ project.py
@@ -1,1 +1,8 @@
-
+def hist_data_columns(data, columns=data.columns, ncols=4):
+    nrows = len(columns) // ncols + 1
+    fig, axs = plt.subplots(nrows, ncols, figsize=(18,nrows*3))
+    for ax, column in zip(axs.ravel(), columns):
+        data[column].hist(ax=ax)
+        ax.set_title(column)
+    
+    fig.tight_layout()
