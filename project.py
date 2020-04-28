ffcc2131ec7764bb0b92ba578a9ef00c4983d283
Sami Ali <s-sami.sayed@zewailcity.edu.eg>
2020-04-29 01:28:52 +0200

    Add files via upload

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-def hist_data_columns(data, columns, ncols=4):
+def hist_data_columns(data, columns=data.columns, ncols=4):
     nrows = len(columns) // ncols + 1
     fig, axs = plt.subplots(nrows, ncols, figsize=(18,nrows*3))
     for ax, column in zip(axs.ravel(), columns):
@@ -7,7 +7,7 @@ def hist_data_columns(data, columns, ncols=4):
     
     fig.tight_layout()
 
-def handling(df):
+def handlingMissingValues(df):
     df.Alley = df.Alley.fillna(value = 'NoAlley')
     df.BsmtCond = df.BsmtCond.fillna(value = 'NoBsmt')
     df.BsmtQual = df.BsmtQual.fillna(value = 'NoBsmt')
@@ -28,6 +28,42 @@ def handling(df):
     df.MasVnrType = df.MasVnrType.fillna(value = 'None')
     df.MasVnrArea = df.MasVnrArea.fillna(value = 0)
     df.dropna(inplace=True)
-    
-def binned_prices(prices):
-    return [0 if price <= 120000 else 1 if price <= 200000 else 2 for price in prices]
+
+def Heatmap(df):
+    corr = df.corr()
+    fig, ax = plt.subplots(figsize=(12,12))
+    sns.heatmap(corr, ax=ax, cmap='seismic', center= 0.0, square = True);
+
+def top10Heatmap(df):
+    corr = df.corr()
+    cols = corr.nlargest(11, 'SalePrice')['SalePrice'].index 
+    corr = df[cols].corr()
+    fig, ax = plt.subplots(figsize=(12,12))
+    sns.heatmap(corr, ax=ax, xticklabels=cols, yticklabels=cols, annot=True);
+
+def LabelEncoder(df):
+    df.BsmtCond = df.BsmtCond.map({'Ex':5 ,'Gd':4 , 'TA':3 ,'Fa':2 ,'Po':1 , 'NoBsmt':0})
+    df.BsmtQual = df.BsmtQual.map({'Ex':5 ,'Gd':4 , 'TA':3 ,'Fa':2 ,'Po':1 , 'NoBsmt':0})
+    df.BsmtExposure = df.BsmtExposure.map({'Gd':4, 'Av':3, 'Mn':2, 'No':1, 'NoBsmt':0})
+    df.BsmtFinType1 = df.BsmtFinType1.map({'GLQ':6,'ALQ':5,'BLQ':4,'Rec':3,'LwQ':2,'Unf':1,'NoBsmt':0})
+    df.BsmtFinType2 = df.BsmtFinType2.map({'GLQ':6,'ALQ':5,'BLQ':4,'Rec':3,'LwQ':2,'Unf':1,'NoBsmt':0})
+
+    df.GarageType = df.GarageType.map({'BuiltIn':6, 'Attchd': 5, '2Types':4 , 'Basment':3 , 'Detchd':2, 'CarPort' :1 , 'NoGarage': 0})
+    df.GarageCond = df.GarageCond.map({'NoGarage':0, 'Po':1, 'Fa':2, 'TA':3, 'Gd':4, 'Ex':5})
+    df.GarageQual = df.GarageQual.map({'NoGarage':0, 'Po':1, 'Fa':2, 'TA':3, 'Gd':4, 'Ex':5})
+    df.GarageFinish = df.GarageFinish.map({'Fin':3, 'RFn':2, 'Unf':1, 'NoGarage':0})
+    df.PavedDrive = df.PavedDrive.map({'Y':2,'P':1, 'N':0 })
+
+    df.MSZoning = df.MSZoning.map({"RL":4,'RM':3,'RH':2,'C (all)':1,'FV':0})
+    df.Street = df.Street.map({'Pave':0,'Grvl':1})
+    df.Alley = df.Alley.map({'NoAlley':0,'Grvl':1, 'Pave':0})
+    df.LotShape = df.LotShape.map({'Reg':0, 'IR1':1, 'IR2':2, 'IR3':3})
+    df.ExterCond = df.ExterCond.map({"Ex":4,'Gd':3,'TA':2,'Fa':1,'Po':0})
+    df.ExterQual = df.ExterQual.map({"Ex":4,'Gd':3,'TA':2,'Fa':1,'Po':0})
+
+    df.CentralAir = df.CentralAir.map({'Y':1, 'N':0})
+    df.HeatingQC = df.HeatingQC.map({"Ex":4,'Gd':3,'TA':2,'Fa':1,'Po':0})
+    df.FireplaceQu = df.FireplaceQu.map({"Ex":5,'Gd':4,'TA':3,'Fa':2,'Po':1, 'Nofireplace':0})
+    df.KitchenQual = df.KitchenQual.map({"Ex":4,'Gd':3,'TA':2,'Fa':1,'Po':0})
+    df.PoolQC = df.PoolQC.map({"Ex":4,'Gd':3,'TA':2,'Fa':1,'NoPool':0})
+    df.Fence = df.Fence.map({'GdPrv':4 , 'MnPrv':3 , 'GdWo':2 , 'MnWw':1 , 'NoFence':0})
